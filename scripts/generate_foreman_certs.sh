16b1234fe8c0cb6f557b44c0bae07ccbc5ffca5f
Ivo Verberk <ivo.verberk@gmail.com>
2014-11-08 17:33:24 +0100

    Use cat to get certs from the container

--- scripts/generate_foreman_certs.sh
+++ scripts/generate_foreman_certs.sh
@@ -8,9 +8,9 @@ mkdir -p private_keys
 docker exec -t puppetmaster puppet cert generate foreman.localdomain
 
 # Copy the certificates from the container
-docker cp puppetmaster:/var/lib/puppet/ssl/certs/ca.pem certs/
-docker cp puppetmaster:/var/lib/puppet/ssl/certs/foreman.localdomain.pem certs/
-docker cp puppetmaster:/var/lib/puppet/ssl/private_keys/foreman.localdomain.pem private_keys/
+docker exec -t puppetmaster cat /var/lib/puppet/ssl/certs/ca.pem > certs/ca.pem
+docker exec -t puppetmaster cat /var/lib/puppet/ssl/certs/foreman.localdomain.pem > certs/foreman.localdomain.pem
+docker exec -t puppetmaster cat /var/lib/puppet/ssl/private_keys/foreman.localdomain.pem > private_keys/foreman.localdomain.pem
 
 # Inject the certificates into the Foreman container
 docker exec -t foreman cp -f /host/certs/ca.pem /var/lib/puppet/ssl/certs/ca.pem
