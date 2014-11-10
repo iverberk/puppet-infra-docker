d4bf5b73af76512223d2b19cdc9ee8af82175b18
Ivo Verberk <ivo.verberk@gmail.com>
2014-11-11 00:21:06 +0100

    Change basemodule path

--- packer/manifests/puppetmaster.pp
+++ packer/manifests/puppetmaster.pp
@@ -23,14 +23,21 @@ node default {
     require => Exec['foreman-installer']
   }
 
+  # Fix the common module path
+  file_line { 'puppetmaster_common_modules':
+    path    => '/etc/puppet/puppet.conf',
+    line    => '    basemodulepath   = /etc/puppet/environments/common/modules:/etc/puppet/modules:/usr/share/puppet/modules',
+    match   => '    basemodulepath.*',
+    require => Exec['foreman-installer']
+  }
+
   class { 'hiera':
     eyaml     => true,
+    datadir   => '/etc/puppet/hiera',
     hierarchy => [
       '%{::environment}/nodes/%{::clientcert}',
       '%{::environment}/modules/%{module_name}',
-      '%{::environment}/vagrant',
-      '%{::environment}/common',
-      '%{::environment}/secrets'
+      '%{::environment}/common'
     ]
   }
 
