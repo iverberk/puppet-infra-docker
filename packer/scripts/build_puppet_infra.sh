d8e68271a2840217e6cb19b8aac91bc677230938
Ivo Verberk <ivo.verberk@gmail.com>
2014-11-22 14:05:47 +0100

    Make the local domain name and Docker registry name configurable

--- packer/scripts/build_puppet_infra.sh
+++ packer/scripts/build_puppet_infra.sh
@@ -1,43 +1,66 @@
 #!/bin/bash -e
 
+if [ -z "$1" ]; then
+	echo "Please supply a local domain for the containers"
+	exit 1
+fi
+
+if [ -z "$2" ]; then
+	echo "Please provide a name for the images (NAME/puppetdb:packer, NAME/puppetmaster:packer, etc.)"
+	exit 1
+fi
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR/.."
+
+DOMAIN=$1
+NAME=$2
+
+FOREMAN=foreman.json
+PUPPETDB=puppetdb.json
+PUPPETMASTER=puppetmaster.json
+
+sed -r -i "s/-h=(.*\.).*\"(.*)$/-h=\1$DOMAIN\"\2/g" $FOREMAN $PUPPETDB $PUPPETMASTER
+sed -r -i "s/CERT=(.*\.).*\"/CERT=\1$DOMAIN\"/g" $FOREMAN $PUPPETDB
+
 echo -e "Building Puppetmaster image..."
-packer build puppetmaster.json
+packer build $PUPPETMASTER
 
 echo -e "Importing artifact..."
-docker import - iverberk/puppetmaster:packer < puppetmaster.tar > /dev/null
+docker import - $NAME/puppetmaster:packer < puppetmaster.tar > /dev/null
 
 echo -e "Generating certificates for PuppetDB and Foreman..."
 
-docker run --name="foreman_cert" -t iverberk/puppetmaster:packer puppet cert generate foreman.dev.local > /dev/null
-docker commit foreman_cert iverberk/puppetmaster:packer > /dev/null
+docker run --name="foreman_cert" -t $NAME/puppetmaster:packer puppet cert generate foreman.$DOMAIN > /dev/null
+docker commit foreman_cert $NAME/puppetmaster:packer > /dev/null
 
-docker run --name="puppetdb_cert" -t iverberk/puppetmaster:packer puppet cert generate puppetdb.dev.local > /dev/null
+docker run --name="puppetdb_cert" -t $NAME/puppetmaster:packer puppet cert generate puppetdb.$DOMAIN > /dev/null
 
 echo -e "Copying certificates from the container..."
 mkdir -p certs
 mkdir -p private_keys
 
 docker cp puppetdb_cert:/var/lib/puppet/ssl/certs/ca.pem certs/
-docker cp puppetdb_cert:/var/lib/puppet/ssl/certs/foreman.dev.local.pem certs/
-docker cp puppetdb_cert:/var/lib/puppet/ssl/certs/puppetdb.dev.local.pem certs/
+docker cp puppetdb_cert:/var/lib/puppet/ssl/certs/foreman.$DOMAIN.pem certs/
+docker cp puppetdb_cert:/var/lib/puppet/ssl/certs/puppetdb.$DOMAIN.pem certs/
 
-docker cp puppetdb_cert:/var/lib/puppet/ssl/private_keys/foreman.dev.local.pem private_keys/
-docker cp puppetdb_cert:/var/lib/puppet/ssl/private_keys/puppetdb.dev.local.pem private_keys/
+docker cp puppetdb_cert:/var/lib/puppet/ssl/private_keys/foreman.$DOMAIN.pem private_keys/
+docker cp puppetdb_cert:/var/lib/puppet/ssl/private_keys/puppetdb.$DOMAIN.pem private_keys/
 
-docker commit puppetdb_cert iverberk/puppetmaster:packer > /dev/null
+docker commit puppetdb_cert $NAME/puppetmaster:packer > /dev/null
 
 docker rm foreman_cert puppetdb_cert > /dev/null
 
 echo -e "Building PuppetDB image..."
-packer build puppetdb.json
+packer build $PUPPETDB
 
 echo -e "Importing artifact..."
-docker import - iverberk/puppetdb:packer < puppetdb.tar > /dev/null
+docker import - $NAME/puppetdb:packer < puppetdb.tar > /dev/null
 
 echo -e "Building Foreman image..."
-packer build foreman.json
+packer build $FOREMAN
 
 echo -e "Importing artifact..."
-docker import - iverberk/foreman:packer < foreman.tar > /dev/null
+docker import - $NAME/foreman:packer < foreman.tar > /dev/null
 
 echo -e "Done!"
